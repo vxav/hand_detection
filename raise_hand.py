9b6f657ad5e9bd7118cf943a845879dfbb2d1288
Xavier <xavier.avrillier@gmail.com>
2025-03-11 23:56:38 +0100

    Update raise_hand.py

--- raise_hand.py
+++ raise_hand.py
@@ -1,7 +1,5 @@
 import cv2
 import mediapipe as mp
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
 # Variables for tracking arm positions
